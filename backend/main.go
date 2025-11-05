00e573511d334e984f263d4a793b5ec2905c79aa
nefimov <n.a.efimov@gmail.com>
2025-11-05 16:32:44 +0100

    add reading BOT_TOKEN from config.env

--- backend/main.go
+++ backend/main.go
@@ -3,20 +3,42 @@ package main
 import (
 	"baton/backend/internal/database"
 	"baton/backend/internal/telegram"
+	"bufio"
+    // "encoding/json"
+    // "io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 var botToken string
 
 func main() {
 	log.Println("Starting application...")
-	botToken = os.Getenv("BOT_TOKEN")
-	if botToken == "" {
-		log.Fatal("BOT_TOKEN required")
-	}
 
+	// Read and parse config.env
+    file, err := os.Open("config.env")
+    if err != nil {
+        log.Fatal("Error opening config.env:", err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := strings.TrimSpace(scanner.Text())
+        if strings.HasPrefix(line, "BOT_TOKEN=") {
+            botToken = strings.TrimPrefix(line, "BOT_TOKEN=")
+            break
+        }
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatal("Error reading config.env:", err)
+    }
+    if botToken == "" {
+        log.Fatal("BOT_TOKEN not found in config.env")
+    }
+	
 	//Initialize database
 	log.Println("Initializing database...")
 	database.InitDB("./baTON.db") // Creates baTON.db in project root
